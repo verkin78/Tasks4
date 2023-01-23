f62f2c5afbfdd981a0f77dc2b7096ea45c784e3c
Verkin78 <otrajenie.jc@gmail.com>
2023-01-23 19:26:34 +0300

    Tasks4.1 - 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
 
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> arrayList = new ArrayList<>();
+        T min = null;
+        T max = null;
+        arrayList = stream
+                .sorted(order)
+                .collect(Collectors.toList());
+        if (arrayList.size() != 0) {
+            min = arrayList.get(0);
+            max = arrayList.get(arrayList.size() - 1);
+            minMaxConsumer.accept(min, max);
+            if (min == null && max == null) {
+                minMaxConsumer.accept(null, null);
+            }
+        }
+    }
 }
